512bb60c64da5be73fc602f4ab173d94e36d2023
Eliot <484900039@qq.com>
2024-04-20 10:27:19 +0800

    Add 108

--- exercises/algorithm/algorithm8.rs
+++ exercises/algorithm/algorithm8.rs
@@ -2,7 +2,6 @@
 	queue
 	This question requires you to use queues to implement the functionality of the stac
 */
-// I AM NOT DONE
 
 #[derive(Debug)]
 pub struct Queue<T> {
@@ -68,14 +67,57 @@ impl<T> myStack<T> {
     }
     pub fn push(&mut self, elem: T) {
         //TODO
+        // self.q1.enqueue(elem);
+
+        // Move all elements from q1 to q2
+        while let Ok(value) = self.q1.dequeue() {
+            self.q2.enqueue(value);
+        }
+
+        // Enqueue the new element to q1
+        self.q1.enqueue(elem);
+
+        // Move all elements back from q2 to q1
+        while let Ok(value) = self.q2.dequeue() {
+            self.q1.enqueue(value);
+        }
     }
     pub fn pop(&mut self) -> Result<T, &str> {
         //TODO
-		Err("Stack is empty")
+        // // 如果q1为空，返回错误
+        // if self.q1.is_empty() {
+        //     return Err("Stack is empty");
+        // }
+
+        // // 如果q1中只有一个元素，直接出队返回即可
+        // if self.q1.size() == 1 {
+        //     return self.q1.dequeue();
+        // }
+
+        // // 将q1中的元素依次出队并入队到q2，直到q1中只剩下一个元素
+        // while self.q1.size() > 1 {
+        //     if let Ok(elem) = self.q1.dequeue() {
+        //         self.q2.enqueue(elem);
+        //     }
+        // }
+
+        // // 将q1中剩下的最后一个元素出队，即为栈顶元素
+        // let result = self.q1.dequeue().unwrap(); // 在这里使用 unwrap 是安全的，因为上面已经确保了 q1 不为空
+
+        // // 交换 q1 和 q2 的引用，使得 q1 始终为空队列
+        // std::mem::swap(&mut self.q1, &mut self.q2);
+
+        // // 返回栈顶元素
+        // Ok(result)
+
+        if self.q1.is_empty() {
+            return Err("Stack is empty");
+        }
+		self.q1.dequeue()
     }
     pub fn is_empty(&self) -> bool {
 		//TODO
-        true
+        self.q1.is_empty()
     }
 }
 
